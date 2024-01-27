69325b69ec611bc65d44d9124f558eee9ffb416f
Eason Ong <eong@lcs.on.ca>
2024-01-27 10:01:29 -0500

    Created detail view and organized project by creating Views group.

---
+++ Landmarks/Views/DetailViews.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViews.swift
+//  Landmarks
+//
+//  Created by Ong Eason on 27/1/2024.
+//
+
+import SwiftUI
+
+struct DetailViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailViews()
+}
